05c11348321ce67ac7ce9f1aa5fad8e4cf4d31b1
Richard Deloge <richarddeloge@gmail.com>
2025-03-19 12:28:18 +0100

    Fix security.xml

--- Security/Authenticator/Oauth2Authenticator.php
+++ Security/Authenticator/Oauth2Authenticator.php
@@ -95,7 +95,7 @@ class Oauth2Authenticator extends AbstractAuthenticator
 
             $accessTokenBadge = new AccessTokenBadge($accessToken, $roles);
 
-            return new SelfValidatingPassport(new UserBadge($client->getUserIdentifier()), [$accessTokenBadge]);
+            return new SelfValidatingPassport(new UserBadge($client->getPublicId()), [$accessTokenBadge]);
         } catch (OAuth2ServerException $e) {
             throw new AuthenticationException('OAuth2 authentication failed', 0, $e);
         }
